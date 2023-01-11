45aa28494f20babb36e705088ad0f260628cf860
Jose <diazmarinjoseelias2007@gmail.com>
2023-01-11 12:12:01 -0500

    Upgrade

--- src/Components/Tooltip.jsx
+++ src/Components/Tooltip.jsx
@@ -2,8 +2,7 @@ const Tooltip = ({icon, text}) => {
     return (
         <div className="group flex relative">
             {icon}
-            <span class="group-hover:opacity-100 transition-opacity bg-gray-900 px-2 text-xs text-gray-100 rounded-md absolute left-1/2 top-2 
--translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">{text}</span>
+            <span class="group-hover:opacity-100 transition-opacity bg-gray-900 p-1 text-xs text-gray-100 rounded-md absolute left-1/2 top-2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">{text}</span>
         </div>
     )
 }
