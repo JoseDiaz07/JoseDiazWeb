f27b98c67f707620b39c4df6c0c3f2f6ff90cf87
Jose <diazmarinjoseelias2007@gmail.com>
2023-12-24 12:25:49 -0500

    Upgrade

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,100 +1,193 @@
-import { useState } from "react";
-import { NavLink, useLocation, useNavigate } from "react-router-dom"
-import { HiMoon, HiSun } from "react-icons/hi2";
-import { MdMenu, MdClose } from "react-icons/md";
-import { useToggleTheme } from "../hooks/useToggleTheme";
+import { useState } from 'react'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { HiMoon, HiSun } from 'react-icons/hi2'
+import { MdMenu, MdClose } from 'react-icons/md'
+import { useToggleTheme } from '../hooks/useToggleTheme'
 
 export const Navbar = () => {
-    const location = useLocation()
-    const [navbar, setNavbar] = useState(false);
-    const { theme, toggleTheme } = useToggleTheme()
+	const location = useLocation()
+	const [navbar, setNavbar] = useState(false)
+	const { theme, toggleTheme } = useToggleTheme()
 
-    const [Icon, setIcon] = useState(false)
+	const [Icon, setIcon] = useState(false)
 
-    const toggleIcon = () => theme === 'dark' ? setIcon(false) : setIcon(true)
+	const toggleIcon = () => (theme === 'dark' ? setIcon(false) : setIcon(true))
 
-    const navigate = useNavigate()
-    const transitionPage = (ev, url) => {
-        ev.preventDefault();
-        if (document.startViewTransition) {
-            document.startViewTransition(() => {
-                navigate(url);
-            })
-        } else {
-            navigate(url)
-        }
-    }
+	const navigate = useNavigate()
+	const transitionPage = (ev, url) => {
+		ev.preventDefault()
+		if (document.startViewTransition) {
+			document.startViewTransition(() => {
+				navigate(url)
+			})
+		} else {
+			navigate(url)
+		}
+	}
 
-    return (
-        <>
-            <nav className="bg-zinc-900 dark:bg-slate-100 text-white dark:text-black w-full pt-5 md:pt-12  transition-all duration-500 px-5 md:px-20">
-                <div className="flex justify-between text-xl ">
-                    <NavLink to="/"><div className="flex hover:text-red-500 cursor-pointer transition-colors ">
-                        <img src="/img/me-min.jpg" alt="" className="rounded-full w-14 mr-3 h-14 object-cover scale-110" />
-                        <div>
-                            <h1 className="font-semibold text-1xl duration-100">José Díaz</h1>
-                            <p className="text-sm text-gray-400">Dev</p>
-                        </div>
-                    </div>
-                    </NavLink>
-                    <div className="hidden gap-x-10 self-end md:flex pb-1">
-                        <NavLink to="/" className={location.pathname === "/" ? "h-8 nav" : "text-gray-500 nav"}>
-                            <h1 className="mt-0.5 duration-200" onClick={(e) => transitionPage(e, "/")}><span className="font-semibold font-mono">01</span> Home</h1>
-                            {
-                                location.pathname === "/" && <div className=" bg-red-800 h-[3px] w-[100%] left-0 -bottom-[5px] rounded-lg  relative"></div>
-                            }
-                        </NavLink>
+	return (
+		<>
+			<nav className='bg-zinc-900 dark:bg-slate-100 text-white dark:text-black w-full pt-5 md:pt-12  transition-all duration-500 px-5 md:px-20'>
+				<div className='flex justify-between text-xl '>
+					<NavLink to='/'>
+						<div className='flex hover:text-red-500 cursor-pointer transition-colors '>
+							<img
+								src='/img/me-min.jpg'
+								alt=''
+								className='rounded-full w-14 mr-3 h-14 object-cover scale-110'
+							/>
+							<div>
+								<h1 className='font-semibold text-1xl duration-100'>
+									José Díaz
+								</h1>
+								<p className='text-sm text-gray-400'>Dev</p>
+							</div>
+						</div>
+					</NavLink>
+					<div className='hidden gap-x-10 self-end md:flex pb-1'>
+						<NavLink
+							to='/'
+							className={
+								location.pathname === '/' ? 'h-8 nav' : 'text-gray-500 nav'
+							}
+						>
+							<h1
+								className='mt-0.5 duration-200'
+								onClick={(e) => transitionPage(e, '/')}
+							>
+								<span className='font-semibold font-mono'>01</span> Home
+							</h1>
+							{location.pathname === '/' && (
+								<div className=' bg-red-800 h-[3px] w-[100%] left-0 -bottom-[5px] rounded-lg  relative'></div>
+							)}
+						</NavLink>
 
-                        <NavLink to="/about" className={location.pathname === "/about" ? " h-8" : "text-gray-500 nav"}>
-                            <h1 className="mt-0.5 duration-200" onClick={(e) => transitionPage(e, "/about")}><span className="font-semibold font-mono">02</span> About</h1>
-                            {
-                                location.pathname === "/about" && <div className="bg-red-800 h-[3px] w-[100%] left-0 -bottom-[5px] rounded-lg  relative"></div>
-                            }
-                        </NavLink>
+						<NavLink
+							to='/about'
+							className={
+								location.pathname === '/about' ? ' h-8' : 'text-gray-500 nav'
+							}
+						>
+							<h1
+								className='mt-0.5 duration-200'
+								onClick={(e) => transitionPage(e, '/about')}
+							>
+								<span className='font-semibold font-mono'>02</span> About
+							</h1>
+							{location.pathname === '/about' && (
+								<div className='bg-red-800 h-[3px] w-[100%] left-0 -bottom-[5px] rounded-lg  relative'></div>
+							)}
+						</NavLink>
 
-                        {
-                            Icon
-                                ? <span className="hidden md:block rounded-full bg-slate-700 p-2 cursor-pointer hover:bg-slate-600 w-auto h-10 dark:bg-slate-300 hover:origin-center hover:-rotate-12 transition-all duration-200 scale-125" onClick={function () { toggleTheme(); toggleIcon() }}><HiMoon size={24} /></span>
-                                
-                                : <span className="hidden md:block rounded-full bg-slate-700 p-2 cursor-pointer w-auto h-10 dark:bg-slate-300  hover:origin-center hover:-rotate-45 transition-all duration-200 scale-125" onClick={function () { toggleTheme(); toggleIcon() }}><HiSun size={24} /></span>
-                        }
+						{Icon ? (
+							<span
+								className='hidden md:block rounded-full bg-slate-700 p-2 cursor-pointer hover:bg-slate-600 w-auto h-10 dark:bg-slate-300 hover:origin-center hover:-rotate-12 transition-all duration-200 scale-125'
+								onClick={function () {
+									toggleTheme()
+									toggleIcon()
+								}}
+							>
+								<HiMoon size={24} />
+							</span>
+						) : (
+							<span
+								className='hidden md:block rounded-full bg-slate-700 p-2 cursor-pointer w-auto h-10 dark:bg-slate-300  hover:origin-center hover:-rotate-45 transition-all duration-200 scale-125'
+								onClick={function () {
+									toggleTheme()
+									toggleIcon()
+								}}
+							>
+								<HiSun size={24} />
+							</span>
+						)}
+					</div>
+					<div className='flex gap-4 justify-end md:hidden'>
+						{Icon ? (
+							<span
+								className='block md:hidden rounded-full bg-slate-600 p-2 cursor-pointer hover:bg-slate-500 w-auto h-10 dark:bg-slate-300 hover:origin-center hover:-rotate-12 transition-all duration-200 scale-125'
+								onClick={function () {
+									toggleTheme()
+									toggleIcon()
+								}}
+							>
+								<HiMoon size={24} />
+							</span>
+						) : (
+							<span
+								className='block md:hidden rounded-full bg-slate-600 p-2 cursor-pointer hover:bg-slate-500 w-auto h-10 dark:bg-slate-300 hover:origin-center hover:-rotate-45 transition-all duration-200 scale-125'
+								onClick={function () {
+									toggleTheme()
+									toggleIcon()
+								}}
+							>
+								<HiSun size={24} />
+							</span>
+						)}
+						<MdMenu
+							size={38}
+							onClick={() => setNavbar(!navbar)}
+							className='transition-all duration-500 md:hidden rounded-full bg-slate-600 p-2 cursor-pointer dark:bg-slate-300 scale-125'
+						/>
+					</div>
+					{navbar ? (
+						<div
+							className='h-screen w-full top-0 left-0 z-20 bg-[#121212] dark:bg-white fixed  transition-all duration-500 md:hidden'
+							transition-style='in:circle:top-right'
+						>
+							<MdClose
+								size={38}
+								className='absolute right-0 m-4 bg-slate-600 cursor-pointer p-1 dark:bg-slate-300 rounded-full scale-125'
+								onClick={() => setNavbar(!navbar)}
+							/>
+							<div className='pt-44 pl-20 gap-y-10 flex flex-col'>
+								<NavLink
+									to='/'
+									onClick={() => setTimeout(() => setNavbar(!navbar), 200)}
+								>
+									<h1
+										className={
+											location.pathname === '/'
+												? 'h-16 text-6xl font-serif'
+												: 'text-gray-500 nav text-6xl font-serif'
+										}
+									>
+										<span className='bold font-mono text-slate-500 text-2xl'>
+											01
+										</span>{' '}
+										Home
+									</h1>
+									{location.pathname === '/' && (
+										<div className='bg-red-800 h-[3px] w-[75%] left-0 -bottom-[5px] rounded-lg  relative'></div>
+									)}
+								</NavLink>
 
-                    </div>
-                    <div className="flex gap-4 justify-end md:hidden">
-                        {
-                            Icon
-                                ? <span className="block md:hidden rounded-full bg-slate-600 p-2 cursor-pointer hover:bg-slate-500 w-auto h-10 dark:bg-slate-300 hover:origin-center hover:-rotate-12 transition-all duration-200 scale-125" onClick={function () { toggleTheme(); toggleIcon() }}><HiMoon size={24} /></span>
-
-                                : <span className="block md:hidden rounded-full bg-slate-600 p-2 cursor-pointer hover:bg-slate-500 w-auto h-10 dark:bg-slate-300 hover:origin-center hover:-rotate-45 transition-all duration-200 scale-125" onClick={function () { toggleTheme(); toggleIcon() }}><HiSun size={24} /></span>
-                        }
-                        <MdMenu size={38} onClick={() => setNavbar(!navbar)} className="transition-all duration-500 md:hidden rounded-full bg-slate-600 p-2 cursor-pointer dark:bg-slate-300 scale-125" />
-                    </div>
-                    {
-                        navbar
-                            ?
-                            <div className="h-screen w-full top-0 left-0 z-20 bg-[#121212] dark:bg-white fixed  transition-all duration-500 md:hidden" transition-style="in:circle:top-right">
-                                <MdClose size={38} className="absolute right-0 m-4 bg-slate-600 cursor-pointer p-1 dark:bg-slate-300 rounded-full scale-125" onClick={() => setNavbar(!navbar)} />
-                                <div className="pt-44 pl-20 gap-y-10 flex flex-col">
-                                    <NavLink to="/" onClick={() => setTimeout(() => setNavbar(!navbar), 200)}>
-                                        <h1 className={location.pathname === "/" ? "h-16 text-6xl font-serif" : "text-gray-500 nav text-6xl font-serif"}><span className="bold font-mono text-slate-500 text-2xl">01</span> Home</h1>
-                                        {
-                                            location.pathname === "/" && <div className="bg-red-800 h-[3px] w-[75%] left-0 -bottom-[5px] rounded-lg  relative"></div>
-                                        }
-                                    </NavLink>
-
-                                    <NavLink to="/about" onClick={() => setTimeout(() => setNavbar(!navbar), 200)}>
-                                        <h1 className={location.pathname === "/about" ? "h-16 text-6xl font-serif " : "text-gray-500 nav text-6xl font-serif "}><span className="bold font-mono text-slate-500 text-2xl">02</span> About</h1>
-                                        {
-                                            location.pathname === "/about" && <div className=" bg-red-800 h-[3px] w-[75%] left-0 -bottom-[5px] rounded-lg  relative"></div>
-                                        }
-                                    </NavLink>
-                                </div>
-                            </div>
-                            :
-                            ""
-                    }
-                </div>
-            </nav>
-        </>
-    )
+								<NavLink
+									to='/about'
+									onClick={() => setTimeout(() => setNavbar(!navbar), 200)}
+								>
+									<h1
+										className={
+											location.pathname === '/about'
+												? 'h-16 text-6xl font-serif '
+												: 'text-gray-500 nav text-6xl font-serif '
+										}
+									>
+										<span className='bold font-mono text-slate-500 text-2xl'>
+											02
+										</span>{' '}
+										About
+									</h1>
+									{location.pathname === '/about' && (
+										<div className=' bg-red-800 h-[3px] w-[75%] left-0 -bottom-[5px] rounded-lg  relative'></div>
+									)}
+								</NavLink>
+							</div>
+						</div>
+					) : (
+						''
+					)}
+				</div>
+			</nav>
+		</>
+	)
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,56 +1,178 @@
-import { Link } from "react-router-dom"
-import Tooltip from "../Components/Tooltip";
-import { AiFillHtml5 } from "react-icons/ai";
-import { DiCss3 } from "react-icons/di";
-import { IoLogoJavascript, IoLogoNodejs } from "react-icons/io";
-import { IoLogoReact } from "react-icons/io5";
-import { FaBootstrap } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
-import { ProjectCard } from "../Components/ProjectCard";
-import { happySpace, knowty, morseTranslator } from "../data/projectTechnologies";
+import { Link } from 'react-router-dom'
+import Tooltip from '../Components/Tooltip'
+import { AiFillHtml5 } from 'react-icons/ai'
+import { DiCss3 } from 'react-icons/di'
+import { IoLogoJavascript, IoLogoNodejs } from 'react-icons/io'
+import { IoLogoReact } from 'react-icons/io5'
+import { FaBootstrap } from 'react-icons/fa'
+import { SiTailwindcss } from 'react-icons/si'
+import { ProjectCard } from '../Components/ProjectCard'
+import {
+	happySpace,
+	knowty,
+	morseTranslator,
+} from '../data/projectTechnologies'
+import { useEffect, useRef } from 'react'
 
 export const Home = () => {
-    return (
-        <div className="px-1 bg-zinc-900 transition-all duration-500 dark:bg-slate-100 min-h-screen">
-            <div className="pt-12 px-4 md:pt-28 md:px-16 transition-all duration-500">
-                <Link to="/about">
-                    <h3 className="text-red-500 font-semibold font-sans text-lg md:text-2xl hover:text-red-700 transition-all duration-300 md:w-96" data-aos="fade-right" data-aos-duration="800" data-aos-once="true">HELLO WORLD, I AM JOSE DIAZ</h3>
-                </Link>
-                <h1 className="text-4xl md:text-8xl text-white dark:text-black font-serif pt-2 pb-6 " data-aos="fade-right" data-aos-duration="800" data-aos-once="true">Code and websites.</h1>
-                <h5 className="text-xl  md:text-3xl  text-white dark:text-black font-sans font-light" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
-                    A front-end developer and creator of <a href="https://knowty.colegiometropolitanodesoledad2000.edu.co/" className="underline underline-offset-8     decoration-red-400  decoration-solid transition-all duration-200 hover:bg-red-400 hover:text-white tracking-tight" target="_blank" rel="noreferrer">Knowty</a>, an educational page for tenth grade students. <br className="hidden md:block" /> I also like to help people and contribute to humanity.
-                </h5>
-            </div>
+	const skillsRef = useRef()
 
-            <div className="pt-20 pb-10 md:pt-56 md:pb-20">
-                <div className="mx-4 py-43 p-3 md:mx-auto  rounded-lg md:px-16 md:py-14  md:max-w-4xl bg-[#28292a] dark:bg-white dark:text-black dark:shadow-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
-                    <h1 className="text-4xl md:text-6xl text-white dark:text-black text-center font-medium font-serif tracking-wider">Skills</h1>
-                    <p className="text-lg md:text-xl text-white dark:text-black text-center font-light mt-2 w-full md:w-[24em] mx-auto">I am currently proficient in these technologies.</p>
+	const mouseMoveEvent = (e) => {
+		skillsRef.current.style.setProperty('--x', e.clientX / window.innerWidth)
+		skillsRef.current.style.setProperty('--y', e.clientY / window.innerHeight)
+	}
 
-                    <div className="flex gap-3 md:gap-20 justify-center mt-6 md:mt-4">
-                        <Tooltip icon={<AiFillHtml5 size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="HTML" />
-                        <Tooltip icon={<DiCss3 size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="CSS" />
-                        <Tooltip icon={<IoLogoJavascript size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="Javascript" />
-                        <Tooltip icon={<IoLogoNodejs size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="Node" />
-                        <Tooltip icon={<IoLogoReact size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="React" />
-                        <Tooltip icon={<FaBootstrap size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="Bootstrap" />
-                        <Tooltip icon={<SiTailwindcss size={36} className="text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200" />} text="Tailwind" />
-                    </div>
-                </div>
-            </div>
+	useEffect(() => {
+		const element = skillsRef.current
+		if (element) {
+			element.addEventListener('mousemove', mouseMoveEvent)
+		}
 
-            <div className="pb-20 pt-12 px-4 md:pt-24 md:px-20">
-                <h1 className="text-5xl text-white dark:text-black font-serif pt-2 tracking-wide">Portfolio</h1>
+		return () => {
+			if (element) {
+				element.removeEventListener('mousemove', mouseMoveEvent)
+			}
+		}
+	}, [skillsRef])
 
-                
-                <div className="flex flex-wrap justify-center mt-10 md:gap-x-10 gap-y-10">
-                    <ProjectCard {...knowty}/>
+	return (
+		<div className='px-1 bg-zinc-900 transition-all duration-500 dark:bg-slate-100 min-h-screen '>
+			<div className='pt-12 px-4 md:pt-28 md:px-16 transition-all duration-500'>
+				<Link to='/about' className='w-fit'>
+					<h3
+						className='text-red-500 font-semibold font-sans text-lg md:text-2xl hover:text-red-700 transition-all duration-300 md:w-fit '
+						data-aos='fade-right'
+						data-aos-duration='800'
+						data-aos-once='true'
+					>
+						HELLO WORLD, I AM JOSE DIAZ
+					</h3>
+				</Link>
+				<h1
+					className='text-4xl md:text-8xl text-white dark:text-black font-serif pt-2 pb-6 '
+					data-aos='fade-right'
+					data-aos-duration='800'
+					data-aos-once='true'
+				>
+					Code and websites.
+				</h1>
+				<h5
+					className='text-xl  md:text-3xl  text-white dark:text-black font-sans font-light'
+					data-aos='fade-right'
+					data-aos-duration='1000'
+					data-aos-once='true'
+				>
+					A front-end developer and creator of{' '}
+					<a
+						href='https://knowty.colegiometropolitanodesoledad2000.edu.co/'
+						className='underline underline-offset-8     decoration-red-400  decoration-solid transition-all duration-200 hover:bg-red-400 hover:text-white tracking-tight'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Knowty
+					</a>
+					, an educational page for tenth grade students.{' '}
+					<br className='hidden md:block' /> I also like to help people and
+					contribute to humanity.
+				</h5>
+			</div>
 
-                    <ProjectCard {...happySpace}/>
+			<div className='pt-20 pb-10 md:pt-56 md:pb-20'>
+				<div
+					className='mx-4 py-43 p-3 md:mx-auto  rounded-lg md:px-16 md:py-14  md:max-w-4xl bg-[#28292a] dark:bg-white dark:text-black dark:shadow-lg shinyDark dark:shiny overflow-hidden'
+					data-aos='fade-up'
+					data-aos-duration='1000'
+					data-aos-once='true'
+					ref={skillsRef}
+				>
+					<h1 className='text-4xl md:text-6xl text-white dark:text-black text-center font-medium font-serif tracking-wider'>
+						Skills
+					</h1>
+					<p className='text-lg md:text-xl text-white dark:text-black text-center font-light mt-2 w-full md:w-[24em] mx-auto'>
+						I am currently proficient in these technologies.
+					</p>
 
-                    <ProjectCard {...morseTranslator}/>
-                </div>
-            </div>
-        </div>
-    )
+					<div className='flex gap-3 md:gap-20 justify-center mt-6 md:mt-4'>
+						<Tooltip
+							icon={
+								<AiFillHtml5
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='HTML'
+						/>
+						<Tooltip
+							icon={
+								<DiCss3
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='CSS'
+						/>
+						<Tooltip
+							icon={
+								<IoLogoJavascript
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='Javascript'
+						/>
+						<Tooltip
+							icon={
+								<IoLogoNodejs
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='Node'
+						/>
+						<Tooltip
+							icon={
+								<IoLogoReact
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='React'
+						/>
+						<Tooltip
+							icon={
+								<FaBootstrap
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='Bootstrap'
+						/>
+						<Tooltip
+							icon={
+								<SiTailwindcss
+									size={36}
+									className='text-white dark:text-[#495057] hover:text-gray-400 dark:hover:text-gray-500 transition-all duration-200'
+								/>
+							}
+							text='Tailwind'
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div className='pb-20 pt-12 px-4 md:pt-24 md:px-20'>
+				<h1 className='text-5xl text-white dark:text-black font-serif pt-2 tracking-wide'>
+					Portfolio
+				</h1>
+
+				<div className='flex flex-wrap justify-center mt-10 md:gap-x-10 gap-y-10'>
+					<ProjectCard {...knowty} />
+
+					<ProjectCard {...happySpace} />
+
+					<ProjectCard {...morseTranslator} />
+				</div>
+			</div>
+		</div>
+	)
 }
